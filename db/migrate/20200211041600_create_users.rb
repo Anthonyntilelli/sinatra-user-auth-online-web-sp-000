2e7d844426f3a01d7f2f4b9c776c525fff405f37
Anthony Tilelli <anthony@tilelli.me>
2020-02-11 04:21:33 +0000

    Automatically backed up by Learn

--- db/migrate/20200211041600_create_users.rb
+++ db/migrate/20200211041600_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.
+      t.string :password
+    end
       
   end
 end
